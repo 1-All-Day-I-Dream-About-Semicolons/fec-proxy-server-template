9571c513ed84e8b0befe43ecbb5c7208812a7858
Ricky Marasigan <rickymarasigan@Rickys-MacBook-Pro.local>
2020-11-24 15:35:27 -0800

    displaying services 1 and 2 on page

--- server/config/services.js
+++ server/config/services.js
@@ -42,23 +42,23 @@
 
 module.exports = {
   service1: {
-    api: 'FIX_ME',
+    api: '/products/1',
     url: 'http://localhost:3001',
-    bundle: 'app.js',
+    bundle: '/products/1/bundle.js'
   },
   service2: {
-    api: 'FIX_ME',
-    url: 'http://localhost:3002',
-    bundle: 'app.js',
-  },
-  service3: {
-    api: 'FIX_ME',
-    url: 'http://localhost:3003',
-    bundle: 'app.js',
-  },
-  service4: {
-    api: 'FIX_ME',
-    url: 'http://localhost:3004',
-    bundle: 'app.js',
-  },
+    api: '/',
+    url: 'http://localhost:3005',
+    bundle: 'bundle.js',
+  }
+  // service3: {
+  //   api: 'FIX_ME',
+  //   url: 'http://localhost:3003',
+  //   bundle: 'app.js',
+  // },
+  // service4: {
+  //   api: 'FIX_ME',
+  //   url: 'http://localhost:3004',
+  //   bundle: 'app.js',
+  // },
 };
